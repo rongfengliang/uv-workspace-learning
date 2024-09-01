b956f92020520d79892c857b315b099aa5477998
rongfengliang <1141591465@qq.com>
2024-09-01 10:02:10 +0800

    add more

--- src/restapi/hello.py
+++ src/restapi/hello.py
@@ -1,8 +1,11 @@
 from login import login
 from api import add
 from common import hello
-if __name__ == "__main__":
+
+def main():
     result = login("admin", "admin")
     add_result = add(1, 2)
     hello_result = hello()
     print(result,add_result,hello_result)
+if __name__ == "__main__":
+    main()
